541ad5e1ce24a0432d6fa5bc6d9ea6ce12c591aa
Kirill Ionkin <kirill-ionkin@mail.ru>
2021-05-29 06:22:37 +0300

    end of changed README.md

--- hw1/sqrtx.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        eps = 1e-3
-        y = 1.0
-
-        while abs(y ** 2 - x) > eps:
-            y = (y + x / y) / 2
-
-        return int(y)
---
+++ hw_1/valid-parentheses.py
@@ -0,0 +1,39 @@
+import pytest
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        same_brackets = {")": "(", "]": "[", "}": "{"}
+        open_brackets = ("(", "[", "{")
+        stack = list()
+
+        for bracket in s:
+            if bracket in open_brackets:
+                stack.append(bracket)
+            else:
+                try:
+                    open_bracket = stack.pop()
+                    if open_bracket != same_brackets[bracket]:
+                        return False
+                except IndexError:
+                    return False
+
+        return False if len(stack) else True
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("", True),
+        ("[]", True),
+        ("{}", True),
+        ("()", True),
+        ("{{{}}}", True),
+        ("[([)]]", False),
+        ("((())))", False),
+        ("(({)})", False),
+        ("[][][]{}{}{}()()()", True),
+    ],
+)
+def test_isvalid(test_input, expected):
+    assert Solution().isValid(test_input) is expected
